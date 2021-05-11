87c70567a8790447eac54c088a5666b1f4d11b80
Scott Adams <sradms0@protonmail.com>
2021-05-11 13:25:53 -0400

    update bivariate choropleth coloring to range from light yellow to dark blue

--- src/layer_operations.py
+++ src/layer_operations.py
@@ -42,15 +42,15 @@ def style_geo_layers():
     print('styling geo layers')
 
     COLORS = {
-        '1.1': '#f3f3f3',
-        '1.2': '#eac5dd',
-        '1.3': '#e6a3d0',
-        '2.1': '#c2f1ce',
-        '2.2': '#9ec6d3',
-        '2.3': '#bc9fce',
-        '3.1': '#8be2af',
-        '3.2': '#7fc6b1',
-        '3.3': '#7b8eaf'
+        '1.1': '#fffdf0',
+        '1.2': '#e6f1e0',
+        '1.3': '#d4e5f7',
+        '2.1': '#fdf3ab',
+        '2.2': '#bfdebe',
+        '2.3': '#a1c7eb',
+        '3.1': '#e6d201',
+        '3.2': '#7ec6b0',
+        '3.3': '#007fc4'
     }
 
     state_unemp_sui_geo_layers = get_geo_layers(QgsProject.instance().mapLayers())
